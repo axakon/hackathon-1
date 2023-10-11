3a617d0b350e2e2758e1821cc67c44c45a054855
Felix Eklöf <felixeklof@icloud.com>
2023-10-11 15:28:28 +0200

    forbidden words check

--- prompt-script.ts
+++ prompt-script.ts
@@ -1,52 +1,71 @@
-import openai from "openai";
+import openai, { OpenAI } from "openai";
 import * as fs from "fs";
 import consola from "consola";
 
-const openAiApiKey = process.env.OPEN_AI_API_KEY;
-
-if (openAiApiKey === undefined) {
-  consola.error("OPEN_AI_API_KEY is missing from .env");
-  process.exit(1);
+function createOpenAiClient() {
+  const openAiApiKey = process.env.OPEN_AI_API_KEY;
+  if (openAiApiKey === undefined) {
+    throw new Error("OPEN_AI_API_KEY is missing from .env");
+  }
+  return new OpenAI({
+    apiKey: openAiApiKey,
+  });
 }
 
-const openAiClient = new openai.OpenAI({
-  apiKey: openAiApiKey,
-});
-
-let promptBuffer: Buffer;
-try {
+function loadPrompt() {
+  let promptBuffer: Buffer;
   promptBuffer = fs.readFileSync("./prompt.txt");
-} catch (e) {
-  consola.error("Could not read 'prompt.txt'.");
-  console.error(e);
-  process.exit(1);
+  return promptBuffer.toString();
 }
 
-const prompt = promptBuffer.toString();
-
-export const messages: openai.Chat.ChatCompletionMessageParam[] = [
-  {
-    role: "user",
-    content: prompt,
-  },
-];
-
-consola.info("Loading");
-const response = await openAiClient.chat.completions.create({
-  messages,
-  model: "gpt-3.5-turbo",
-});
-
-const content = response.choices[0]?.message.content;
-if (!content) {
-  consola.error("Chat completion failed, got no message back.");
-  process.exit(1);
+async function getChatCompletion(client: OpenAI, prompt: string) {
+  const messages: openai.Chat.ChatCompletionMessageParam[] = [
+    {
+      role: "user",
+      content: prompt,
+    },
+  ];
+  const response = await client.chat.completions.create({
+    messages,
+    model: "gpt-3.5-turbo",
+  });
+  return response.choices[0]?.message.content;
 }
-if (!fs.existsSync("./public")) {
-  fs.mkdirSync("./public");
+
+function doesPromptContainForbiddenWord(prompt: string) {
+  const forbiddenPattern = /tic.+tac.+toe/;
+  return forbiddenPattern.test(prompt.toLowerCase());
 }
-fs.writeFileSync("./public/index.html", content);
 
-consola.success("Result written to index.html");
+function writeIndexHtml(content: string) {
+  if (!fs.existsSync("./public")) {
+    fs.mkdirSync("./public");
+  }
+  fs.writeFileSync("./public/index.html", content);
+}
 
-process.exit(0);
+async function run() {
+  const client = createOpenAiClient();
+  const prompt = loadPrompt();
+  const hasForbiddenWord = doesPromptContainForbiddenWord(prompt);
+  if(hasForbiddenWord) {
+    throw new Error("Prompt is not allowed to contain any combination of word 'tic-tac-toe'.")
+  }
+  consola.info("Loading");
+  const chatResponse = await getChatCompletion(client, prompt);
+  if (!chatResponse) {
+    throw new Error("Chat completion failed, got no message back.");
+  }
+  writeIndexHtml(chatResponse);
+}
+
+if (process.argv[2] === "exec") {
+  try {
+    await run();
+  } catch(e) {
+    consola.error(e);
+    process.exit(1);
+  }
+  consola.success("Result written to index.html");
+  process.exit(0);
+}
